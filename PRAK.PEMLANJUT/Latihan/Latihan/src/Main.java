eeba5cea58c9f73fb00ceaff805a0005d4997307
yaasmin26 <yaasminramadhani@gmail.com>
2025-10-31 12:40:35 +0700

    intial commit - pemesanan tiket dengan javadoc

---
+++ PRAK.PEMLANJUT/Latihan/Latihan/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Kelas Main digunakan untuk menjalankan program pemesanan tiket film.
+ */
+public class Main {
+    /**
+     *Method utama program.
+     * @param args argumen baris perintah
+     */
+    public static void main(String[] args) {
+        PemesananTiket tiket = new PemesananTiket("Avengers: Endgame", 3, 50000);
+        tiket.tampilkanDetailTiket();
+    }
+}
+
